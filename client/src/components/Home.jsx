b606a4f7856eab7228702b09ca1252e94102b0ce
dzambr13 <dayvuhz@gmail.com>
2022-09-13 17:46:26 -0700

    css

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,21 +1,15 @@
 const Home = () => {
 
   return (
-    <div className="home">
+    <div className="homepage">
       <div>
       <img className="homeimg"
-      style={{ maxWidth: '50%'}}
       src="https://i.pinimg.com/originals/98/28/6a/98286a2243ada070aef5a6925dd3bc54.gif"
       alt="logo"
       />
     </div>
     <div>
-      <h1>Welcome! Go ahead an explore my Tattoo Blog<br/></h1>
-    </div>
-    <br></br>
-    <div>
-      <p>Here you can view other people's posts, or you can make your own.<br/></p>
-      <p>Go ahead and click on <em>Categories</em> to get a group of different tattoo posts!<br /></p>
+      <h1 className="homeint">Stay awhile!</h1>
     </div>
     </div>    
   )
